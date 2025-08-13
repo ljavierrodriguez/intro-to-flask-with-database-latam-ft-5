4ece5fcc010da419e47c90a53c038eb0a0f40348
Luis J. Rodriguez O <ljavierrodriguez@gmail.com>
2025-08-13 11:43:15 -0400

    Relationship Flask

--- src/app.py
+++ src/app.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, jsonify, request
 from flask_migrate import Migrate
 from flask_cors import CORS
-from models import db, Task
+from models import db, Task, User, Profile, Course
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -23,7 +23,7 @@ def get_todos():
     todos = Task.query.all() # SELECT * FROM todos => [<Task 1>, <Task 2>]
 
     # Convertir a diccionario cada uno de los objetos en la lista de tareas
-    todos = [task.convertir_a_dict() for task in todos]
+    todos = [task.serialize() for task in todos]
     #todos = list(map(lambda task: task.convertir_a_dict(), todos))
 
     return jsonify(todos), 200
@@ -42,7 +42,7 @@ def add_task():
     db.session.add(task)
     db.session.commit() # Guardamos en la base de datos definitivamente
 
-    return jsonify(task.convertir_a_dict()), 201
+    return jsonify(task.serialize()), 201
 
 @app.route('/todos/<int:id>', methods=['DELETE'])
 def delete_task(id):
@@ -56,6 +56,17 @@ def delete_task(id):
     db.session.commit() # Guardamos la eliminacion permanentemente
 
     return jsonify({ "success": "Tarea eliminada correctamente"}), 200
+
+@app.route('/info/<int:user_id>', methods=['GET'])
+def info_usuario(user_id):
+
+    user = User.query.get(user_id)
+
+    if not user:
+        return jsonify({"error": "Usuario no existe"}), 404
+
+    return jsonify(user.serialize()), 200
+
 
 if __name__ == '__main__':
     app.run()
--- src/models.py
+++ src/models.py
@@ -1,14 +1,88 @@
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
+users_courses = db.Table(
+    "users_courses",
+    db.Column("users_id", db.Integer, db.ForeignKey('users.id'), primary_key=True),
+    db.Column("courses_id", db.Integer, db.ForeignKey('courses.id'), primary_key=True),
+)
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(200), nullable=False)
+
+    # 1-1 Un usuario tiene un perfil
+    profile = db.relationship('Profile', back_populates="user", uselist=False, cascade="all, delete-orphan")
+
+    # 1-M Un usuario tiene muchas tareas
+    todos = db.relationship('Task', back_populates="user", cascade="all, delete-orphan")
+
+    # M-N Un usuario puede estar en muchos cursos
+    courses = db.relationship('Course', secondary=users_courses, back_populates="users")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email
+        }
+
+    def serialize_complete_info(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            "profile": {
+                "bio": self.profile.bio,
+                "github": self.profile.github
+            },
+            "todos": [task.serialize() for task in self.todos],
+            "courses": [course.serialize() for course in self.courses]
+        }
+
+class Profile(db.Model):
+    __tablename__ = 'profiles'
+    id = db.Column(db.Integer, primary_key=True)
+    bio = db.Column(db.Text(), default="")
+    github = db.Column(db.String(100), default="")
+    users_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    # relationship back con usuarios
+    user = db.relationship('User', back_populates="profile")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "bio": self.bio,
+            "github": self.github
+        }
+    
+class Course(db.Model):
+    __tablename__ = 'courses'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+
+    # M-N Un curso puede estar asociado a muchos usuarios
+    users = db.relationship('User', secondary=users_courses, back_populates="courses")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name
+        }
+
+
 class Task(db.Model):
     __tablename__ = 'todos'
 
     id = db.Column(db.Integer, primary_key=True)
     label = db.Column(db.String(200), nullable=False)
     done = db.Column(db.Boolean, default=False)
+    users_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
-    def convertir_a_dict(self):
+    # relationship back con usuarios
+    user = db.relationship('User', back_populates="todos")
+
+    def serialize(self):
         return {
             "id": self.id,
             "label": self.label,
